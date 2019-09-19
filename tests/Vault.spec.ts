4b57cda8ddd7117c1409710f135524ccc954abd3
Jerome Küttner <j.kuettner@mittwald.de>
2019-09-19 15:21:10 +0200

    fix test in ci

--- tests/Vault.spec.ts
+++ tests/Vault.spec.ts
@@ -1,4 +1,4 @@
-import {IVaultConfig, Vault} from "../src/Vault";
+import {IVaultConfig, Vault} from "../src";
 
 describe("Vault Integration Test (requires running vault instance)", () => {
     test("successfully queries running dev vault", async () => {
